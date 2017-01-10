d7b6358b8523855cbff0b7c7d819552644ae3de6
calvin cushman and Jayeson Kiyabu <calvin@>
2017-01-10 10:19:21 -0800

    add javascript toggle animation to all imgs

--- architecture/js/scripts.js
+++ architecture/js/scripts.js
@@ -1,3 +1,14 @@
 $(function() {
-  
-}
+  $(".click1").click(function() {
+    $(".college-hidden").toggle();
+    $(".college-showing").toggle();
+  });
+  $(".click2").click(function() {
+    $(".stonehenge-showing").fadeToggle();
+    $(".stonehenge-hidden").fadeToggle();
+  });
+  $(".click3").click(function() {
+    $(".opera-showing").slideToggle();
+    $(".opera-hidden").slideToggle();
+  });
+});
